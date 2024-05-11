3f4d41d795624186bb3f3b5ccdea04be1d9c745e
Ardstand <73349677+Ardstand@users.noreply.github.com>
2024-05-11 06:27:00 +0530

    Create firebaseCon.js

---
+++ config/firebaseCon.js
@@ -0,0 +1,21 @@
+// use this format
+
+/* const config = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+}; */
+
+export const config = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+};
